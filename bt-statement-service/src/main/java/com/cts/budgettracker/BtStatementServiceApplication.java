e987ac05a042e5b2b7d12fc19722f65a95f7cf68
narendrakl <narendraklr@gmail.com>
2022-01-27 23:33:29 +0530

    added txns service skeleton

---
+++ bt-statement-service/src/main/java/com/cts/budgettracker/BtStatementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cts.budgettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtStatementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtStatementServiceApplication.class, args);
+	}
+
+}
---
+++ bt-txns-service/src/test/java/com/cts/budgettracker/BtTxnsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.budgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtTxnsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
